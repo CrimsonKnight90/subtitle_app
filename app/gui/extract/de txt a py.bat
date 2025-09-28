c9e10433cbd3019ad3326432b574199fee68664b
CrimsonKnight90 <hernandezjaenvangelys@gmail.com>
2025-09-27 22:13:35 -0400

    Remove .bat files from repository

--- app/gui/extract/de txt a py.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-echo Cambiando extensiones .txt a .py en la carpeta actual...
-for %%f in (*.txt) do (
-    ren "%%f" "%%~nf.py"
-)
-echo Listo.
--- app/gui/translate/de py a txt.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-echo Cambiando extensiones .py a .txt en la carpeta actual...
-for %%f in (*.py) do (
-    ren "%%f" "%%~nf.txt"
-)
-echo Listo.
